65b056c23e18631825378d4e0ce69856abe92581
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2024-09-20 16:14:56 -0500

    AGR-18 search view started

--- src/datasets/views.py
+++ src/datasets/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import DatasetPage
 
-# Create your views here.
+def search(request):
+    search_query = request.GET.get('query', None)
+    datasets = DatasetPage.objects.live()
+
+    # Filtros por ubicación, tamaño, frecuencia, etc.
+    if 'location' in request.GET:
+        datasets = datasets.filter(location__icontains=request.GET['location'])
+    # Agrega más filtros según sea necesario
+
+    return render(request, 'datasets/search_results.html', {
+        'search_query': search_query,
+        'datasets': datasets,
+    })
