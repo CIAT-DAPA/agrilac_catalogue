0868fb721ccd7fcc84b352fddfefaf7e85d36f5a
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2024-09-23 11:55:45 -0500

    AGR-18 search view list of datasets implemented

---
+++ src/datasets/static/js/search_results.js
@@ -0,0 +1,7 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     document.getElementById("toggle-filters").addEventListener("click", function () {
+//       var sidebar = document.getElementById("sidebar-filters");
+//       sidebar.classList.toggle("d-none"); // Cambia 'd-none' por 'bg-success' para probar
+//     });
+//   });
+  
--- src/home/templatetags/custom_filters.py
+++ src/home/templatetags/custom_filters.py
@@ -1,4 +1,6 @@
 from django import template
+from wagtail.rich_text import RichText
+import re
 
 register = template.Library()
 
@@ -6,3 +8,14 @@ register = template.Library()
 def split(value, arg):
     """Divide el valor de la cadena por el argumento especificado"""
     return value.split(arg)
+
+@register.filter
+def remove_empty_tags(value):
+    """
+    Elimina etiquetas vacías o con solo espacios en el contenido HTML.
+    """
+    if value:
+        # Remover etiquetas vacías o con solo espacios
+        cleaned_value = re.sub(r'<(p|div|h[1-6]|span)[^>]*>\s*(<br/>)?\s*</\1>', '', str(value), flags=re.MULTILINE)
+        return cleaned_value.strip()  # Remover espacios adicionales al final o principio
+    return value
