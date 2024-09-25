8ca87c9e10c97721a1f7df80399bbb2c02102207
Sebastian Lopez <sebastian.lopez_ast@uao.edu.co>
2024-09-25 10:55:53 -0500

    AGR-18 search and filters working

--- src/datasets/static/js/search_results.js
+++ src/datasets/static/js/search_results.js
@@ -2,11 +2,40 @@ document.addEventListener("DOMContentLoaded", function () {
   const searchInput = document.getElementById("searchInput");
   const resultsContainer = document.getElementById("resultsContainer");
 
-  // Función para realizar la búsqueda
+  // Función para realizar la búsqueda con filtros
   function search(query, page = 1) {
-    fetch(
-      `/search/searchDataset/?query=${encodeURIComponent(query)}&page=${page}`
-    )
+    const fechaInicio = document.getElementById("floatingStart").value;
+    const fechaFin = document.getElementById("floatingEnd").value;
+    const ubicaciones = getCheckedValues(
+      '#location-filter input[type="checkbox"]'
+    );
+    const variables = getCheckedValues(
+      '#variables-filter input[type="checkbox"]'
+    );
+    const instituciones = getCheckedValues(
+      '#institution-filter input[type="checkbox"]'
+    );
+    const accesos = getCheckedValues('#acces-filter input[type="checkbox"]');
+    const palabrasClaves = getCheckedValues(
+      '#key-words-filter input[type="checkbox"]'
+    );
+    const frecuencias = getCheckedValues(
+      '#frequency-filter input[type="checkbox"]'
+    );
+
+    const filtrosSeleccionados = {
+      fechaInicio,
+      fechaFin,
+      ubicaciones,
+      variables,
+      instituciones,
+      accesos,
+      palabrasClaves,
+      frecuencias,
+    };
+
+    const params = buildQueryParams(filtrosSeleccionados, query, page);
+    fetch(`/search/searchDataset/${params}`)
       .then((response) => response.text())
       .then((data) => {
         resultsContainer.innerHTML = data; // Actualiza el contenedor de resultados
@@ -14,14 +43,6 @@ document.addEventListener("DOMContentLoaded", function () {
       })
       .catch((error) => console.error("Error:", error));
   }
-
-  // Realiza la búsqueda cuando la página se cargue
-  search(searchInput.value);
-
-  // También realiza la búsqueda cuando se ingresa texto en el input
-  searchInput.addEventListener("input", function () {
-    search(searchInput.value);
-  });
 
   // Función para manejar los clics en la paginación
   function attachPaginationHandlers() {
@@ -36,126 +57,45 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // Llamar la función inicialmente para configurar los handlers de paginación
-  attachPaginationHandlers();
-});
+  // Función para obtener los valores seleccionados de los checkboxes
+  function getCheckedValues(selector) {
+    return Array.from(document.querySelectorAll(`${selector}:checked`)).map(
+      (checkbox) => checkbox.value
+    );
+  }
 
-document.addEventListener("DOMContentLoaded", function () {
+  // Función para construir los parámetros de la consulta
+  function buildQueryParams(filters, query, page) {
+    const params = [`query=${encodeURIComponent(query)}`, `page=${page}`];
+
+    if (filters.fechaInicio) params.push(`start_date=${filters.fechaInicio}`);
+    if (filters.fechaFin) params.push(`end_date=${filters.fechaFin}`);
+    if (filters.ubicaciones.length > 0)
+      params.push(`region_name=${filters.ubicaciones.join(",")}`);
+    if (filters.variables.length > 0)
+      params.push(`variable_name=${filters.variables.join(",")}`);
+    if (filters.instituciones.length > 0)
+      params.push(`institution=${filters.instituciones.join(",")}`);
+    if (filters.accesos.length > 0)
+      params.push(`type_dataset=${filters.accesos.join(",")}`);
+    if (filters.palabrasClaves.length > 0)
+      params.push(`keywords=${filters.palabrasClaves.join(",")}`);
+    if (filters.frecuencias.length > 0)
+      params.push(`upload_frequency=${filters.frecuencias.join(",")}`);
+
+    return `?${params.join("&")}`;
+  }
+
+  // Inicializa la búsqueda y los handlers de paginación
+  search(searchInput.value);
+  searchInput.addEventListener("input", () => search(searchInput.value));
+  attachPaginationHandlers();
+
   // Captura los cambios en los filtros
   const filters = document.querySelectorAll(
     '.form-check-input, input[type="date"]'
   );
-
-  filters.forEach((filter) => {
-    filter.addEventListener("change", applyFilters);
-  });
-
-  function applyFilters() {
-    // Recoge los valores de las fechas
-    const fechaInicio = document.getElementById("floatingStart").value;
-    const fechaFin = document.getElementById("floatingEnd").value;
-
-    // Filtros de ubicación (paises)
-    const ubicaciones = Array.from(
-      document.querySelectorAll(
-        '#location-filter input[type="checkbox"]:checked'
-      )
-    ).map((checkbox) => checkbox.value);
-
-    // Filtros de variables
-    const variables = Array.from(
-      document.querySelectorAll(
-        '#variables-filter input[type="checkbox"]:checked'
-      )
-    ).map((checkbox) => checkbox.value);
-
-    // Filtros de instituciones
-    const instituciones = Array.from(
-      document.querySelectorAll(
-        '#institution-filter input[type="checkbox"]:checked'
-      )
-    ).map((checkbox) => checkbox.value);
-
-    // Filtros de acceso
-    const accesos = Array.from(
-      document.querySelectorAll('#acces-filter input[type="checkbox"]:checked')
-    ).map((checkbox) => checkbox.value);
-
-    // Filtros de palabras claves
-    const palabrasClaves = Array.from(
-      document.querySelectorAll(
-        '#key-words-filter input[type="checkbox"]:checked'
-      )
-    ).map((checkbox) => checkbox.value);
-
-    // Filtros de frecuencia de subida
-    const frecuencias = Array.from(
-      document.querySelectorAll(
-        '#frequency-filter input[type="checkbox"]:checked'
-      )
-    ).map((checkbox) => checkbox.value);
-
-    // Crea el objeto con los filtros seleccionados
-    const filtrosSeleccionados = {
-      fechaInicio,
-      fechaFin,
-      ubicaciones,
-      variables,
-      instituciones,
-      accesos,
-      palabrasClaves,
-      frecuencias,
-    };
-    console.log(filtrosSeleccionados);
-
-    function search(filtrosSeleccionados) {
-      const params = [];
-
-      if (filtrosSeleccionados.fechaInicio) {
-        params.push(`start_date=${filtrosSeleccionados.fechaInicio}`);
-      }
-      if (filtrosSeleccionados.fechaFin) {
-        params.push(`end_date=${filtrosSeleccionados.fechaFin}`);
-      }
-      if (filtrosSeleccionados.ubicaciones.length > 0) {
-        params.push(
-          `region_name=${filtrosSeleccionados.ubicaciones.join(",")}`
-        );
-      }
-      if (filtrosSeleccionados.variables.length > 0) {
-        params.push(
-          `variable_name=${filtrosSeleccionados.variables.join(",")}`
-        );
-      }
-      if (filtrosSeleccionados.instituciones.length > 0) {
-        params.push(
-          `institution=${filtrosSeleccionados.instituciones.join(",")}`
-        );
-      }
-      if (filtrosSeleccionados.accesos.length > 0) {
-        params.push(`type_dataset=${filtrosSeleccionados.accesos.join(",")}`);
-      }
-      if (filtrosSeleccionados.palabrasClaves.length > 0) {
-        params.push(
-          `keywords=${filtrosSeleccionados.palabrasClaves.join(",")}`
-        );
-      }
-      if (filtrosSeleccionados.frecuencias.length > 0) {
-        params.push(
-          `upload_frequency=${filtrosSeleccionados.frecuencias.join(",")}`
-        );
-      }
-
-      const queryString = params.length > 0 ? `?${params.join("&")}` : "";
-
-      fetch(`/search/searchDataset/${queryString}`)
-        .then((response) => response.text())
-        .then((data) => {
-          resultsContainer.innerHTML = data;
-        })
-        .catch((error) => console.error("Error:", error));
-    }
-
-    search(filtrosSeleccionados);
-  }
+  filters.forEach((filter) =>
+    filter.addEventListener("change", () => search(searchInput.value))
+  );
 });
