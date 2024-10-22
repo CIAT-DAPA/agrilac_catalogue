1b8c314f671152918b65bb98cc4d288fc2b3f189
David Garcia Gonzalez <deividgrgo88@gmail.com>
2024-10-22 11:34:36 -0600

    Cambios para API

--- src/api_web/urls.py
+++ src/api_web/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from . import post_view
 
 urlpatterns = [
     path('protected/', views.my_protected_view, name='my_protected_view'),
+    path('post-data/', post_view.handle_post_data, name='handle_post_data'),
 ]
